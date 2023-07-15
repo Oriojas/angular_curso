88be5539b309ccbbeb20bff7a2614760a0e8fef9
oscar50 <oscar@whaleandjaguar.co>
2023-07-14 21:14:27 -0500

    feat: create automatic component

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -1,11 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-contador', // etiqueta para llamar el componente
-    templateUrl: './contador.component.html', 
-    styleUrls: ['./contador.component.css'] // se puede linkear con mas css
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
 })
-
 export class ContadorComponent {
 
 }
