60963d5b676a7b8078e1edd2b5902d99b3e2ead9
oscar50 <oscar@whaleandjaguar.co>
2023-07-14 21:07:43 -0500

    feat: create manual component

---
+++ src/app/contador/contador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-contador', // etiqueta para llamar el componente
+    templateUrl: './contador.component.html', 
+    styleUrls: ['./contador.component.css'] // se puede linkear con mas css
+})
+
+export class ContadorComponent {
+
+}
