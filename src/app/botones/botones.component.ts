ba5fb2595c0755e35dbf30b3c6ef70f2c0032523
oscar50 <oscar@whaleandjaguar.co>
2023-07-19 10:10:23 -0500

    feat: change properties of images

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -7,8 +7,6 @@ import { Component } from '@angular/core';
 })
 export class BotonesComponent {
 
-  text_color:string = "";
-
-  button_disabled:boolean = false;
+  src:string = "https://images.pexels.com/photos/16819484/pexels-photo-16819484/free-photo-of-naturaleza-rojo-flores-petalos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
 }
