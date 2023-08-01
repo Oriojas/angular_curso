923dcdd5d02e5a2eae729a75de0a37bab3147b32
oscar50 <oscar@whaleandjaguar.co>
2023-07-31 19:16:41 -0500

    feat: reactive forms

--- src/app/formulario3/formulario3.component.ts
+++ src/app/formulario3/formulario3.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario3',
@@ -8,10 +8,23 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class Formulario3Component {
 
-  // para sincronizar el valor con el front
-  name = new FormControl("", Validators.required);  
-  email = new FormControl("", [Validators.required, Validators.email]);
+  get name(){
+    return this.formUser.get('name') as FormControl; // as FormControl para no colocar le ?. en el html
+  }
 
+  get email (){
+    return this.formUser.get('email') as FormControl;
+  }
 
+  formUser = new FormGroup({
+      // para sincronizar el valor con el front
+    'name': new FormControl('', Validators.required),
+    'email': new FormControl('', [Validators.required, Validators.email]),
+
+  });
+
+  procesar (){
+    console.log(this.formUser.value);
+  }
 
 }
